fe5f9821b17b12f7a6c35d9165e41d4414895dfc
TipleTre <huaguzheng2007@126.com>
2022-10-10 14:47:39 +0800

    feat: update slide to 0.7.5

--- src/App.js
+++ src/App.js
@@ -1,42 +1,152 @@
-import React, {createRef, useCallback, useState} from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Slide, SLIDE_EVENTS } from "@netless/slide";
 import './App.css';
+import {RtcAudioPlayer} from "./RtcAudioPlayer";
 
 function App() {
+    const [useRtc, setUseRtc] = useState(false);
+    const [mode, setMode] = useState("local");
     const [taskId, setTaskId] = useState("06415a307f2011ec8bdc15d18ec9acc7");
     const [prefixUrl, setPrefixUrl] = useState("https://convertcdn.netless.group/dynamicConvert");
 
-    const contentRef = createRef();
+    const anchorA = useRef(null);
+    const slideA = useRef(null);
+    const slideB = useRef(null);
+    const anchorB = useRef(null);
 
-    const startLimitDetect = useCallback(() => {
-        setInterval(() => {
-            if (contentRef.current) {
-                const anchor = document.createElement("div");
-                anchor.style.cssText = "width: 100px;height: 100px;margin: 12px";
-                contentRef.current.appendChild(anchor);
-                const slideObj = new Slide({
-                    anchor: anchor,
-                    interactive: true,
-                    mode: "local",
-                    controller: false,
+    const updateTaskId = useCallback((event) => {
+        setTaskId(event.target.value);
+        localStorage.setItem("slide-taskId", event.target.value);
+    }, []);
+
+    const updatePrefix = useCallback((event) => {
+        setPrefixUrl(event.target.value);
+        localStorage.setItem("slide-prefix", event.target.value);
+    }, []);
+
+    const updateMode = useCallback((event) => {
+        setMode(event.target.value);
+        localStorage.setItem("slide-mode", event.target.value);
+    }, []);
+
+    useEffect(() => {
+        const mode = localStorage.getItem("slide-mode");
+        const taskId = localStorage.getItem("slide-taskId");
+        const prefix = localStorage.getItem("slide-prefix");
+        if (mode) {
+            setMode(mode);
+        }
+        if (taskId) {
+            setTaskId(taskId);
+        }
+        if (prefix) {
+            setPrefixUrl(prefix);
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log("[t]", taskId);
+        if (anchorA.current) {
+            slideA.current = new Slide({
+                anchor: anchorA.current,
+                interactive: true,
+                mode: mode,
+                controller: true,
+                rtcAudio: useRtc ? RtcAudioPlayer : undefined,
+                logger: {
+                    info(msg) {
+                        console.log(msg);
+                    },
+                    warn(msg) {
+                        console.warn(msg);
+                    },
+                    error(msg) {
+                        console.error(msg);
+                    }
+                }
+            });
+            slideA.current.on(SLIDE_EVENTS.stateChange, (s) => {
+                console.log(s);
+            });
+            slideA.current.on(SLIDE_EVENTS.renderError, (err, index) => {
+                console.log(err, index);
+            });
+            if (mode === "sync") {
+                slideA.current.on(SLIDE_EVENTS.syncDispatch, (e) => {
+                    if (slideB.current) {
+                        slideB.current.emit(SLIDE_EVENTS.syncReceive, e);
+                    }
                 });
-                slideObj.setResource(taskId, prefixUrl);
-                slideObj.renderSlide(1);
-                slideObj.on(SLIDE_EVENTS.renderError, (err) => {
-                    console.log(err);
-                    console.log("fjdioasjfoaif")
+            } else if (mode === "interactive") {
+                slideA.current.on(SLIDE_EVENTS.syncDispatch, (e) => {
+                    slideA.current.emit(SLIDE_EVENTS.syncReceive, e);
+                    if (slideB.current) {
+                        slideB.current.emit(SLIDE_EVENTS.syncReceive, e);
+                    }
                 });
             }
-        }, 1000);
-    }, []);
+            slideA.current.setResource(taskId, prefixUrl);
+            slideA.current.renderSlide(1);
+        }
+        return () => {
+            slideA.current?.destroy();
+        };
+    }, [taskId, prefixUrl, mode, useRtc]);
+
+    useEffect(() => {
+        if (mode === "sync" || mode === "interactive") {
+            slideB.current = new Slide({
+                anchor: anchorB.current,
+                interactive: mode !== "sync",
+                mode: mode,
+                controller: true,
+                rtcAudio: useRtc ? RtcAudioPlayer : undefined,
+            });
+            if (mode === "interactive") {
+                slideB.current.on(SLIDE_EVENTS.syncDispatch, (e) => {
+                    slideB.current.emit(SLIDE_EVENTS.syncReceive, e);
+                    if (slideA.current) {
+                        slideA.current.emit(SLIDE_EVENTS.syncReceive, e);
+                    }
+                });
+            }
+            slideB.current.setResource(taskId, prefixUrl);
+            slideB.current.renderSlide(1);
+        }
+        return () => {
+            slideB.current?.destroy();
+        };
+    }, [mode, taskId, prefixUrl, useRtc]);
 
     return (
         <div className="App">
-            <div style={{width: "150px", flex: "0 0 auto" }}>
-                <button onClick={startLimitDetect}>测试 Slide 最大数量</button>
+            <div className={"header"}>
+                <div>
+                    <label>taskId:</label>
+                    <input value={taskId} onChange={updateTaskId}/>
+                    <label>prefixUrl:</label>
+                    <input value={prefixUrl} onChange={updatePrefix}/>
+                </div>
+                <div>
+                    <span>启用 rtc 混音</span>
+                    <input type="checkbox" id="bike" value={useRtc} onChange={() => setUseRtc(!useRtc)}/>
+                    <span>{" "}</span>
+                    <select value={mode} onChange={updateMode}>
+                        <option value={"local"}>本地模式</option>
+                        {/*<option value={"sync"}>同步模式</option>*/}
+                        <option value={"interactive"}>互动模式</option>
+                    </select>
+                </div>
             </div>
-            <div ref={contentRef} className="Content">
-
+            <div className={"content"}>
+                <div className={"anchor"} ref={anchorA} style={{width: mode !== "local" ? "45%" : "90%"}}>
+                    <div onClick={() => slideA.current?.prevStep()} className={"arrow"} style={{left: "0"}}>{"<"}</div>
+                    <div onClick={() => slideA.current?.nextStep()} className={"arrow"} style={{right: "0"}}>{">"}</div>
+                </div>
+                <div className={"anchor"} ref={anchorB} style={{display: mode !== "local" ? "block" : "none", width: mode !== "local" ? "45%" : "100%"}}>
+                    <div onClick={() => slideB.current?.prevStep()} className={"arrow"} style={{left: "0"}}>{"<"}</div>
+                    <div onClick={() => slideB.current?.nextStep()} className={"arrow"} style={{right: "0"}}>{">"}</div>
+                </div>
             </div>
         </div>
     );
